4c41ec5c9445324c8812b38c23846e9a2c90cbb8
ManonChab <godfroymanon@gmail.com>
2026-04-07 23:39:22 +0200

    SCRUM-24 Crear la estructura de base del backend

---
+++ src/test/java/com/verisure/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verisure.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
